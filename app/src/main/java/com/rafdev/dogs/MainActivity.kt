0673e8a57fd9f64b2eaaef0fb3c4362868d7c316
RafDes21 <israeltineo84@gmail.com>
2023-08-24 12:06:30 -0300

    add class APIServices and config fun getRetrofit

--- app/src/main/java/com/rafdev/dogs/MainActivity.kt
+++ app/src/main/java/com/rafdev/dogs/MainActivity.kt
@@ -3,6 +3,8 @@ package com.rafdev.dogs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rafdev.dogs.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
