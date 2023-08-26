880da9f003452216041c8749c8fe2c6af89c1bf5
RafDes21 <israeltineo84@gmail.com>
2023-08-26 15:32:58 -0300

    change version of coroutines

--- app/src/main/java/com/rafdev/dogs/DogAdapter.kt
+++ app/src/main/java/com/rafdev/dogs/DogAdapter.kt
@@ -1,18 +1,19 @@
 package com.rafdev.dogs
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class DogAdapter(val images:List<String>):RecyclerView.Adapter<DogViewHolder>() {
+class DogAdapter(private val images:List<String>):RecyclerView.Adapter<DogViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return DogViewHolder(layoutInflater.inflate(R.layout.item_dog, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int =  images.size
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item:String = images[position]
+        holder.bind(item)
     }
 }
--- app/src/main/java/com/rafdev/dogs/MainActivity.kt
+++ app/src/main/java/com/rafdev/dogs/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rafdev.dogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rafdev.dogs.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,37 +11,64 @@ import kotlinx.coroutines.launch
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), androidx.appcompat.widget.SearchView.OnQueryTextListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: DogAdapter
+    private val dogImages = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.svDogs.setOnQueryTextListener(this)
         initRecyclarView()
     }
 
     private fun initRecyclarView() {
-
+        adapter = DogAdapter(dogImages)
+        binding.rvDogs.layoutManager = LinearLayoutManager(this)
+        binding.rvDogs.adapter = adapter
     }
 
-    private fun getRetrofit():Retrofit{
-
+    private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://dog.ceo/api/breed/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-    private fun searchByName(query:String){
+    private fun searchByName(query: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            val call:Response<DogsResponse> = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val call: Response<DogsResponse> =
+                getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
             val puppies: DogsResponse? = call.body()
-            if (call.isSuccessful){
-
-            }else {
-
+            runOnUiThread {
+                if (call.isSuccessful) {
+                    val images: List<String> = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    showError()
+                }
             }
+
         }
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "ha ocurrido un error", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (!query.isNullOrEmpty()) {
+            searchByName(query.lowercase(Locale.getDefault()))
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 }
